3e5fe163bbbcc49def88ca24b41b5e7f52a367bc
gunner963 <anujoza3@gmail.com>
2018-03-31 02:06:57 -0500

    td form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,35 @@ export class AppComponent {
 defaultQuestion =  'pet';
 answer:string = "";
 genders = ['male', 'female'];
+submitted = false;
+user = {
+  username: '',
+  email: '',
+  secretQuestion: '',
+  answer: '',
+  gender: ''
+}
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = 'Aba123';
+    this.signupForm.form.patchValue(
+      {
+        userData:{
+        userName: suggestedName,
+        }
+      }
+  );
+   
+    /*
+    this.signupForm.setValue({
+      userData:{
+        userName: suggestedName, // using name attribute of html to bind
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });
+*/
   }
   /*
   onSubmit(form: NgForm ){
@@ -21,5 +48,14 @@ genders = ['male', 'female'];
   */
  onSubmit( ){
   console.log(this.signupForm);
+  this.submitted = true;
+  this.user.username = this.signupForm.value.userData.userName;
+  this.user.email = this.signupForm.value.userData.email;
+  this.user.secretQuestion = this.signupForm.value.secret;
+  this.user.answer = this.signupForm.value.questionAnswer;
+  this.user.gender = this.signupForm.value.gender;
+  
+  this.signupForm.reset();
+
 }
 }
